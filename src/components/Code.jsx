188a2a5043130af8517abcdddc8b5b7344389560
DeadInsane5 <105852185+DeadInsane5@users.noreply.github.com>
2025-03-09 18:23:36 +0530

    Added Code

---
+++ src/components/Code.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Code.css"
+import Pattern from './Pattern'
+
+const Code = () => {
+    return (
+        <>
+            <Pattern />
+            <div className='code'>
+                <h1 className='m-2'>Code Puzzle</h1>
+                <p className='text-4xl m-2'>·−·/·/·−−/·−/·−·/−··//··/···//·−−/·−/··/···/−//−·−/··/···/···/·/···</p>
+                <h2 className='text-2xl m-2'>Instructions</h2>
+                <p>
+                    <ol>
+                        <li className='m-2'>1. Solve the above code for a reward</li>
+                        <li className='m-2'>2. Solve within time limit of 6 hours</li>
+                        <li className='m-2'>3. "/" seperates letters, "//" seperates words</li>
+                    </ol>
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Code
